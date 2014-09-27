f59cd3827bcebdaaea5266bd9bc4057190d5aa37
Naotoshi Seo <sonots@gmail.com>
2014-09-27 01:36:19 +0000

    add more go get and chown

--- go/build.sh
+++ go/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo chown -R isucon.isucon /home/isucon/gocode
+go get github.com/sonots/go-sql_metrics
+go get github.com/sonots/go-template_metrics
+go get github.com/sonots/go-http_metrics
+go get github.com/sonots/lltsv
 go get github.com/go-martini/martini
 go get github.com/go-sql-driver/mysql
 go get github.com/martini-contrib/render
